eac09fedb7194b8eaf7fb54b56df47f50cc5eaa8
wahyuragil21 <why.yuragil@gmail.com>
2024-02-13 13:51:20 +0700

    feat : setup for project, install framework and library

---
+++ app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from 'antd';
+import CardComponent from '@/components/card';
+
+const { Meta } = Card;
+
+const Home = () => (
+  <div className="App">
+    <CardComponent />
+  </div>
+);
+
+export default Home;
