b2a05c39c72e609df13fa57930e11a6fd6cf3c3a
wahyuragil21 <why.yuragil@gmail.com>
2024-02-13 17:46:38 +0700

    Refactor : styling component

--- app/src/components/FormComponent.tsx
+++ app/src/components/FormComponent.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 import { FieldNamesType } from 'antd/es/cascader';
+import { usePathname } from 'next/navigation';
 
 type FieldType = {
   email?: string;
@@ -12,38 +13,44 @@ type Props = {
   onSubmit: (data: FieldNamesType) => Promise<void>;
 }
 
-const FormComponent = ({ onSubmit }: Props) => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onSubmit}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="email"
-      name="email"
-      rules={[{ required: true, message: 'Please input your email!' }]}
-    >
-      <Input />
-    </Form.Item>
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
+const FormComponent = ({ onSubmit }: Props) => {
+  const pathname = usePathname()
+  console.log(pathname);
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+  return (
+    <div className='flex flex-col justify-center p-10 border-2 rounded-xl w-96 m-auto mt-20'>
+      <h1 className='text-xl font-bold mb-5'>{pathname == '/login' ? 'Login' : 'Register'}</h1>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ width: 450 }}
+        initialValues={{ remember: true }}
+        onFinish={onSubmit}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Button htmlType="submit" style={{ width: 300 }}>
+          {pathname == '/login' ? 'Login' : 'Register'}
+        </Button>
+
+      </Form>
+    </div>
+  )
+};
 
 export default FormComponent;
