98f5dc9fb908dca21ab60b3494df5673c1d63376
wahyuragil21 <why.yuragil@gmail.com>
2024-02-13 16:55:42 +0700

    Feat : add feature login for user

--- app/src/app/(Auth)/login/page.tsx
+++ app/src/app/(Auth)/login/page.tsx
@@ -1,10 +1,28 @@
-"use client";
+import FormComponent from '@/components/FormComponent';
+import { FieldNamesType } from 'antd/es/cascader';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
 
+const handleLogin = async (data: FieldNamesType) => {
+  "use server";
+  const response = await fetch('https://reqres.in/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  // const x = await response.json();
+
+  if (response.status == 200) {
+    return redirect('/home');
+  }
+
+};
 const Login = () => (
   <>
-    <h1>Login</h1>
+    <FormComponent onSubmit={handleLogin} />
   </>
 );
 
--- app/src/components/FormComponent.tsx
+++ app/src/components/FormComponent.tsx
@@ -1,36 +1,31 @@
 "use client";
 import React from 'react';
 import { Button, Checkbox, Form, Input } from 'antd';
-
-const onFinish = (values: any) => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
-};
+import { FieldNamesType } from 'antd/es/cascader';
 
 type FieldType = {
-  username?: string;
+  email?: string;
   password?: string;
-  remember?: string;
 };
 
-const FormComponent: React.FC = () => (
+type Props = {
+  onSubmit: (data: FieldNamesType) => Promise<void>;
+}
+
+const FormComponent = ({ onSubmit }: Props) => (
   <Form
     name="basic"
     labelCol={{ span: 8 }}
     wrapperCol={{ span: 16 }}
     style={{ maxWidth: 600 }}
     initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
+    onFinish={onSubmit}
     autoComplete="off"
   >
     <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
+      label="email"
+      name="email"
+      rules={[{ required: true, message: 'Please input your email!' }]}
     >
       <Input />
     </Form.Item>
@@ -41,14 +36,6 @@ const FormComponent: React.FC = () => (
       rules={[{ required: true, message: 'Please input your password!' }]}
     >
       <Input.Password />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{ offset: 8, span: 16 }}
-    >
-      <Checkbox>Remember me</Checkbox>
     </Form.Item>
 
     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
